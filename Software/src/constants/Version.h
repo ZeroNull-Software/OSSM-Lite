f513a71850b8cca19b70e11fb56a1224a66c7636
Dan <dan@daniel-ross.com>
2026-09-06 16:40:05 +0100

    chore: note that VERSION is set by the release workflow

--- Software/src/constants/Version.h
+++ Software/src/constants/Version.h
@@ -1,6 +1,8 @@
 #ifndef OSSM_VERSION_H
 #define OSSM_VERSION_H
 
+// Note: VERSION is overwritten automatically by the ZeroNull Firmware
+// Release workflow at build time.
 #define VERSION "1.5.2"
 
 #endif  // OSSM_VERSION_H
